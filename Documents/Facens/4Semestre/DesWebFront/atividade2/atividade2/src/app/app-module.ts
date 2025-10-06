56565b5cdf6e9bc54772d367d4696f2d8482712d
Éric Beltran <ericbeltrancn@gmail.com>
2025-10-06 19:24:43 -0300

    primeiro commit

---
+++ Documents/Facens/4Semestre/DesWebFront/atividade2/atividade2/src/app/app-module.ts
@@ -0,0 +1,22 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+
+@NgModule({
+  declarations: [
+    App
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
