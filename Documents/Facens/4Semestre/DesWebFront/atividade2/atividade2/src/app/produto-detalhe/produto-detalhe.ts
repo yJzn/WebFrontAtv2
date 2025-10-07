b733400e10ecd0a07c06e02fcb5fabb7b8ffcd6d
Éric Beltran <ericbeltrancn@gmail.com>
2025-10-06 21:40:56 -0300

    finalizando componentes

--- Documents/Facens/4Semestre/DesWebFront/atividade2/atividade2/src/app/produto-detalhe/produto-detalhe.ts
+++ Documents/Facens/4Semestre/DesWebFront/atividade2/atividade2/src/app/produto-detalhe/produto-detalhe.ts
@@ -11,11 +11,18 @@ import { Produto } from '../produto/produto';
 export class ProdutoDetalhe implements OnInit {
 produtoId: number = 0;
 produtoSelecionado: Produto | undefined;
-produtos: Produto[] = [];
+
+produtos: Produto[] = [
+  { id: 1, nome: 'Produto A', descricao: 'Descrição do Produto A', preco: 100, emEstoque: true, produtos: [] },
+  { id: 2, nome: 'Produto B', descricao: 'Descrição do Produto B', preco: 150, emEstoque: false, produtos: [] },
+  { id: 3, nome: 'Produto C', descricao: 'Descrição do Produto C', preco: 200, emEstoque: true, produtos: [] },
+  { id: 4, nome: 'Produto D', descricao: 'Descrição do Produto D', preco: 250, emEstoque: true, produtos: [] },
+  { id: 5, nome: 'Produto E', descricao: 'Descrição do Produto E', preco: 300, emEstoque: false, produtos: [] }
+];
 
   constructor(private route: ActivatedRoute) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.produtoId = Number(this.route.snapshot.params['id']);
     this.produtoSelecionado = this.produtos.find(p => p.id === this.produtoId);
   }
